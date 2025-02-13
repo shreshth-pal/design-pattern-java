65d4179dfb8051a6d3f331ec965d6c6ef7a1b500
Shreshth Pal <shreshthpal@outlook.com>
2025-02-13 23:47:59 +0530

    Controller design done

--- headFirstDesignPattern/CommandPattern.java
+++ headFirstDesignPattern/CommandPattern.java
@@ -2,9 +2,26 @@ package headFirstDesignPattern;
 
 public class CommandPattern {
     public static void main(String[] args) {
-        SimpleRemoteControl simpleRemoteControl= new SimpleRemoteControl(3);
-        Light light =new Light();
-        
+        SimpleRemoteControl simpleRemoteControl= new SimpleRemoteControl(4);
+        Light bedroomLight=new Light("bedroom");
+        Light bathroomLight=new Light("bathroom");
+        CeilingFan drawingRoomFan= new CeilingFan("Drawing room");
+        Setero setero=new Setero();
+
+        simpleRemoteControl.setOnCommand(0, new LightCommandOn(bedroomLight),new LightCommandOff(bedroomLight));
+        simpleRemoteControl.setOnCommand(1, new LightCommandOn(bathroomLight),new LightCommandOff(bathroomLight));
+        simpleRemoteControl.setOnCommand(2,new CeilingFanCommandOn(drawingRoomFan),new CeilingFanCommandOff(drawingRoomFan));
+        simpleRemoteControl.setOnCommand(3,new SeteroCommandOn(setero),new SeteroCommandOff(setero));
+
+        simpleRemoteControl.onButtonPressed(0);
+        simpleRemoteControl.onButtonPressed(1);
+        simpleRemoteControl.onButtonPressed(2);
+        simpleRemoteControl.onButtonPressed(3);
+
+        simpleRemoteControl.offButtonPressed(3);
+        simpleRemoteControl.offButtonPressed(2);
+        simpleRemoteControl.offButtonPressed(1);
+        simpleRemoteControl.offButtonPressed(0);
     }
 }
 
@@ -25,16 +42,21 @@ class noCommand implements  Command{
 
 
 class  Light{
+    String place;
+    
+
+    public Light(String place) {
+        this.place = place;
+    }
 
     void on(){
-        System.out.println("Light is on");
+        System.out.println(place+" Light is on");
     }
 
     void off(){
-        System.out.println("Light is off");
+        System.out.println(place+" Light is off");
     }
 }
-
 class LightCommandOn implements Command{
     Light light;
 
@@ -48,8 +70,136 @@ class LightCommandOn implements Command{
     }
     
 } 
+class LightCommandOff implements Command{
+    Light light;
+
+    public LightCommandOff(Light light) {
+        this.light = light;
+    }
+
+    @Override
+    public void execute() {
+        light.off();
+    }
+    
+} 
+
+class Setero{
+    int vol;
+    String type;
+    boolean isActive;
+
+    Setero(){
+        vol=0;
+        type="none";
+        isActive=false;
+    }
+
+    void on(){
+        isActive=true;
+        setDvd();
+        setVolume(10);
+        System.out.println("Set DvD player at vol 10");
+    }
+    
+    void off(){
+        isActive=false;
+    }
+
+    void setDvd(){
+        type="dvd";
+    }
+
+    void setRadio(){
+        type="radio";
+    }
+
+    void setVolume(int vol){
+        this.vol=vol;
+    }
 
 
+}
+class SeteroCommandOn implements Command{
+    
+    Setero setero;
+    public SeteroCommandOn(Setero setero) {
+        this.setero = setero;
+    }
+    @Override
+    public void execute(){
+        setero.on();
+    }
+}
+class SeteroCommandOff implements Command{
+    
+    Setero setero;
+    public SeteroCommandOff(Setero setero) {
+        this.setero = setero;
+    }
+    @Override
+    public void execute(){
+        setero.off();
+    }
+}
+
+class CeilingFan{
+    boolean isActive;
+    String place;
+    public CeilingFan(String place) {
+        isActive=false;
+        this.place=place;
+    }
+
+    void on(){
+        isActive=true;
+        System.out.println(place +" Fan is on");
+    }
+
+    void off(){
+        isActive=false;
+        System.out.println(place +"Fan is off");
+    }
+
+    
+
+}
+class CeilingFanCommandOn implements  Command{
+    
+    CeilingFan ceilingFan;
+
+    
+
+
+    public CeilingFanCommandOn(CeilingFan ceilingFan) {
+        this.ceilingFan = ceilingFan;
+    }
+
+
+
+
+    @Override
+    public  void execute(){
+        ceilingFan.on();
+    }
+}
+class CeilingFanCommandOff implements  Command{
+    
+    CeilingFan ceilingFan;
+
+    
+
+
+    public CeilingFanCommandOff(CeilingFan ceilingFan) {
+        this.ceilingFan = ceilingFan;
+    }
+
+
+    @Override
+    public  void execute(){
+        ceilingFan.off();
+    }
+}
 class SimpleRemoteControl {
    private Command[] onCommands;
    private Command[] offCommands;
