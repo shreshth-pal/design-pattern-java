e17d620f64afffc7478af171c5de0d3031bc27e0
Shreshth Pal <shreshthpal@outlook.com>
2025-02-19 23:34:14 +0530

    Small changes

--- Lecture8TicTacToe/Board.java
+++ Lecture8TicTacToe/Board.java
@@ -2,6 +2,7 @@ package Lecture8TicTacToe;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Scanner;
 
 public class Board {
     Queue<Player> playerQueue = new LinkedList<>();
@@ -25,12 +26,19 @@ public class Board {
     }
 
     void startGame(){
-
+        Scanner scanner = new Scanner(System.in);
         while(turns>0){
            System.out.println("Current Board is:");
            printBoard();
-           System.out.println("Please select a position and gives its X and Y cooridnates");
            Player player= playerQueue.poll();
+           System.out.println("Please select a position "+player.getName() +" and enter \n X coordinate:");  
+           int x=scanner.nextInt();
+           System.out.println("\n Y coordinate:");
+           int y=scanner.nextInt();
+           
+           if(x< size && y <size && x>=0 && y>=0 && board[x][y].getCellChar()!=Pieces.empty){
+
+           }
 
         }
 
@@ -43,17 +51,17 @@ public class Board {
 
 
 class Cell{
-    char cellChar;
+    Pieces cellChar;
 
     public Cell(){
-        cellChar='|'; 
+        cellChar=Pieces.empty;
     }
 
-    public char getCellChar() {
+    public Pieces getCellChar() {
         return cellChar;
     }
 
-    public void setCellChar(char cellChar) {
+    public void setCellChar(Pieces cellChar) {
         this.cellChar = cellChar;
     }
 
